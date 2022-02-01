37566c423c9b06523c1f1a402db875bc911669b4
Oleksandr Kuchmuk <okuchmuk@gmail.com>
2022-02-01 20:48:13 +0200

    add my name, testing work with project together

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,16 @@ import view.impl.LoginMenu;
 public class Main {
     public static void main(String[] args) {
         new LoginMenu().show();
+
+
+
+
+
+
+
+
+
+        //Oleksandr Kuchmuk
+
     }
 }
