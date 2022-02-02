62441e12df6240118d9900d2c824cd9e44be173d
Oleksandr Kuchmuk <okuchmuk@gmail.com>
2022-02-02 16:34:51 +0200

    Started making menus, made LoginMenu, ProductMenu, UserMainMenu

--- src/main/java/view/impl/ProductMenu.java
+++ src/main/java/view/impl/ProductMenu.java
@@ -2,10 +2,40 @@ package view.impl;
 
 import view.Menu;
 
+import java.util.Scanner;
+
 public class ProductMenu implements Menu {
+    private String[] items = {"1.View list of products", "2.Add products to order ", "3. Make order", "0. Exit"};
+    private Scanner scanner = new Scanner(System.in);
+
 
     @Override
     public void show() {
+        showItems(items);
+
+        System.out.print("\nPlease enter the number of the action point you want to perform: ");
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("тут має сформуватись список продуктів");
+                    break;
+                case 2:
+                    System.out.println("тут має бути функція додавання продуктів в замовлення");
+                    break;
+                case 3:
+                    System.out.println("тут має бути функція 'зробити замовлення'");
+                    break;
+                case 0:
+                    exit();
+                    break;
+            }
+        }
 
     }
 
--- src/main/java/view/impl/UserMainMenu.java
+++ src/main/java/view/impl/UserMainMenu.java
@@ -3,10 +3,36 @@ package view.impl;
 
 import view.Menu;
 
+import java.util.Scanner;
+
 public class UserMainMenu implements Menu {
+    private String[] items = {"1.Products menu", "2.My orders", "0. Exit to previous menu"};
+    private Scanner scanner;
+    private ProductMenu productMenu = new ProductMenu();
+
     @Override
     public void show() {
-        System.out.println("AHhaaa!!");
+        showItems(items);
+
+        System.out.print("\nPlease enter the number of the action point you want to perform: ");
+        scanner = new Scanner(System.in);
+
+        while (true) {
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    productMenu.show();
+                    break;
+                case 2:
+                    System.out.println("тут має бути епрехід до моїх замовлень");
+                    break;
+                case 0:
+                    exit();
+                    break;
+            }
+        }
+//        System.out.println("AHhaaa!!");
     }
 
     @Override
