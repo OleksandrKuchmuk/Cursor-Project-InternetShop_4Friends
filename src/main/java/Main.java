4f2bb3dfbb634919d189329ccb6db5fa7b291583
Oleksandr Kuchmuk <okuchmuk@gmail.com>
2022-02-02 14:51:55 +0200

    Deleted all previous changes where we set up collaboration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,7 @@ public class Main {
     public static void main(String[] args) {
         new LoginMenu().show();
 
-        //Степан Новий тест для гідхабу
 
-
-        // Дмитро   *-*-*-*-*-*-*-*-*
-        // Дмитро   *-*-*-*-*-*-*-*-* друга спроба
-
-
-
-        //Oleksandr Kuchmuk
 
     }
 }
